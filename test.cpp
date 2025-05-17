bd455851920956e9f3704187c3186aa027c6fe41
BuildTools <unconfigured@null.spigotmc.org>
2025-05-17 14:27:52 +0200

    iterator

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include "Linlib.h"
+using namespace Lin;
+
+int main(void) {
+    vec vector = {1, 2, 3, 4};
+    printf("%d\n", vector[-1]);
+    for (auto i : vector) {
+        printf("%d", i);
+    }
+
+    printf("\n");
+
+    mat matrix = {{1, 2, 3}, {4, 5, 6}};
+    printf("%d\n", matrix[-1][-1]);
+    for (auto i : matrix) {
+        printf("%d", i);
+    }
+}
